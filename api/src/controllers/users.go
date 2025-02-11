fb8bd0d4f8e24a71ce0f803561c46ca36e730cba
Matheushols <matheusholls@hotmail.com>
2025-02-10 23:22:04 -0300

    Added a controllers structure with api functions

---
+++ api/src/controllers/users.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+// The CreateUser is used to create an user
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Creating User!"))
+}
+
+// The SearchUsers is used to list all users
+func SearchUsers(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Searching all Users!"))
+}
+
+// The SearchUser is used to get an specific user
+func SearchUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Searching for a specific User!"))
+}
+
+// The EditUser is used to edit an specific user
+func EditUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Editing an specific User!"))
+}
+
+// The DeleteUser is used to delete an specific user
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Deleting an specific User!"))
+}
