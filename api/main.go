7ff6b59ba8a2dae71ece92db3472497f059bc359
Matheushols <matheusholls@hotmail.com>
2025-02-12 00:05:53 -0300

    Included the enviromentes variables to database connection and port to comunicate in api

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -8,9 +9,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Roning the API!")
+	config.Upload()
+
+	fmt.Println(config.ConnectionString)
+	fmt.Println("Running the API!")
 
 	r := router.Generate()
 
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
---
+++ api/src/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	// ConnectionString is a string to connect to the database
+	ConnectionString = ""
+
+	// The port where the api is running
+	Port = 0
+)
+
+// Upload is going to start the environment variables
+func Upload() {
+	var erro error
+
+	if erro = godotenv.Load(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	Port, erro = strconv.Atoi(os.Getenv("API_PORT"))
+	if erro != nil {
+		Port = 9000
+	}
+
+	ConnectionString = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+}
